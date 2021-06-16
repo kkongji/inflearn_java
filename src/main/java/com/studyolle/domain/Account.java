aba74b81e812e2b9fc32ff70dc6bb9fe4ec0b8d7
junyoung.han <hjy0085@naver.com>
2021-06-17 00:46:22 +0900

    init project

---
+++ src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,52 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail; // 스터디 가입신청 결과 수신여부
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail; // 스터디가 갱신된 여부 수신여부
+
+    private boolean studyUpdatedByWeb;
+
+}
